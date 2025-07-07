cb762a5429564deb09555032db2ba33ae73d2a2c
bjquyum <a.ahmed@straj.com>
2025-07-07 15:27:07 +0100

    Cors Config

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 
+
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 
 // Configure SMTP transporter (example uses Gmail, replace with your SMTP details)
